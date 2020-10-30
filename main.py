ac390bee7e5f077a228f4af782ac305ac7ba62d4
HillaPeter <peterhilla@gmail.com>
2020-10-30 14:04:37 +0200

    risk factors into csv

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ import matplotlib.pyplot as plt
 
 #Full data
 dfOp = pd.read_csv("/mnt/nadavrap-students/STS/data/Shapira_1st-Op_6_9_20_.csv")
-groupOp = dfOp.groupby("SiteID")["SiteID"].count().reset_index(name='countOp')
+groupOp = dfOp.groupby("SiteID")["SiteID"].count().reset_index(name='countFirst')
 #draw a plot
-x = groupOp["countOp"]
+x = groupOp["countFirst"]
 plt.hist(x, bins=40)
 plt.title("Histogram of count Operation")
 plt.xlabel('number of Operations')
@@ -61,11 +61,11 @@ plt.show()
 ##merge two dataframes into one and gets the ratio between them
 result = pd.merge(groupOp, groupReOp, on='SiteID', how='left')
 result['countReOp'].fillna(0, inplace=True)
-result["countReOp/countOp"] =  (result["countReOp"] /(result["countReOp"]+ result["countOp"])) *100
-result['countReOp/countOp'].fillna(0, inplace=True)
-result.to_csv("/tmp/pycharm_project_723/result.csv")
+result["countReOp/countFirst+countReOp"] =  (result["countReOp"] /(result["countReOp"]+ result["countFirst"])) *100
+result['countReOp/countFirst+countReOp'].fillna(0, inplace=True)
+# result.to_csv("/tmp/pycharm_project_355/result.csv")
 #draw a plot
-z = result['countReOp/countOp']
+z = result['countReOp/countFirst+countReOp']
 plt.hist(z, bins=40)
 plt.title("Histogram of  ReOperation vs Operation")
 plt.xlabel('% of ReOperation of Operation')
@@ -85,19 +85,89 @@ MortaltyReOp = dfReOp.groupby("SiteID")["Mortalty"].count().reset_index(name='Mo
 result2 = pd.merge(MortaltyOp, MortaltyReOp, on='SiteID', how='left')
 # result.merge(result2, on='SiteID')
 df=pd.merge(result, result2, on='SiteID')
-df["totalOp"] = result["countReOp"]+ result["countOp"]
+df["countOpr"] = result["countReOp"]+ result["countFirst"]
 df.to_csv("/tmp/pycharm_project_355/mortalty.csv")
 
 
 
+
+####AGE
+
+ageOp = dfOp.groupby("SiteID")["Age"].mean().reset_index(name='Mean_Age_op')
+ageReOp = dfReOp.groupby("SiteID")["Age"].mean().reset_index(name='Mean_Age_reOp')
+resultAge = pd.merge(ageOp, ageReOp, on='SiteID', how='left')
+dfAge=pd.merge(result, resultAge, on='SiteID')
+
+# ##gender
+# tmpMaleDf=dfOp[dfOp['Gender'] == 1]
+# print(tmpMaleDf)
+# tmpFemaleDf=dfOp[dfOp['Gender'] == 2]
+# resultGender = pd.merge(tmpMaleDf, tmpFemaleDf, on='SiteID', how='left')
+# dfGender=pd.merge(dfAge, resultGender, on='SiteID')
+#
+
+maleOp = dfOp.groupby("SiteID")["Gender"].count().reset_index(name='Male_op')
+maleReOp = dfReOp.groupby("SiteID")["Gender"].count().reset_index(name='Male_reOp')
+resultMale = pd.merge(maleOp, maleReOp, on='SiteID', how='left')
+dfMale=pd.merge(dfAge, resultMale, on='SiteID')
+
+femaleOp = dfOp.groupby("SiteID")["Gender"].count().reset_index(name='Female_op')
+femaleReOp = dfReOp.groupby("SiteID")["Gender"].count().reset_index(name='Female_reOp')
+resultFemale= pd.merge(femaleOp, femaleReOp, on='SiteID', how='left')
+dfFemale=pd.merge(dfMale, resultFemale, on='SiteID')
+
+
+##FHCAD - family history of disease
+FHCADOp = dfOp.groupby("SiteID")["FHCAD"].count().reset_index(name='FHCAD_op')
+FHCADReOp = dfReOp.groupby("SiteID")["FHCAD"].count().reset_index(name='FHCAD_reOp')
+resultFHCAD = pd.merge(FHCADOp, FHCADReOp, on='SiteID', how='left')
+dfFHCAD =pd.merge(dfFemale, resultFHCAD, on='SiteID')
+
+##Hypertn - blood preasure
+HypertnOp = dfOp.groupby("SiteID")["Hypertn"].count().reset_index(name='Hypertn_op')
+HypertnReOp = dfReOp.groupby("SiteID")["Hypertn"].count().reset_index(name='Hypertn_reOp')
+resultHypertn = pd.merge(HypertnOp, HypertnReOp, on='SiteID', how='left')
+dfHypertn =pd.merge(dfFHCAD, resultHypertn, on='SiteID')
+
+##Diabetes
+DiabetesOp = dfOp.groupby("SiteID")["Diabetes"].count().reset_index(name='Diabetes_op')
+DiabetesReOp = dfReOp.groupby("SiteID")["Diabetes"].count().reset_index(name='Diabetes_reOp')
+resultDiabetes = pd.merge(DiabetesOp, DiabetesReOp, on='SiteID', how='left')
+dfDiabetes =pd.merge(dfHypertn, resultDiabetes, on='SiteID')
+
+##Dyslip
+DyslipOp = dfOp.groupby("SiteID")["Dyslip"].count().reset_index(name='Dyslip_op')
+DyslipReOp = dfReOp.groupby("SiteID")["Dyslip"].count().reset_index(name='Dyslip_reOp')
+resultDyslip = pd.merge(DyslipOp, DyslipReOp, on='SiteID', how='left')
+dfDyslip =pd.merge(dfDiabetes, resultDyslip, on='SiteID')
+
+##TobaccoUse
+TobaccoUseOp = dfOp.groupby("SiteID")["TobaccoUse"].count().reset_index(name='TobaccoUse_op')
+TobaccoUseReOp = dfReOp.groupby("SiteID")["TobaccoUse"].count().reset_index(name='TobaccoUse_reOp')
+resultTobaccoUse = pd.merge(TobaccoUseOp, TobaccoUseReOp, on='SiteID', how='left')
+dfTobaccoUse =pd.merge(dfDyslip, resultTobaccoUse, on='SiteID')
+
+##Cancer
+CancerOp = dfOp.groupby("SiteID")["Cancer"].count().reset_index(name='Cancer_op')
+CancerReOp = dfReOp.groupby("SiteID")["Cancer"].count().reset_index(name='Cancer_reOp')
+resultCancer = pd.merge(CancerOp, CancerReOp, on='SiteID', how='left')
+dfCancer =pd.merge(dfTobaccoUse, resultCancer, on='SiteID')
+
+##PVD
+PVDOp = dfOp.groupby("SiteID")["PVD"].count().reset_index(name='PVD_op')
+PVDReOp = dfReOp.groupby("SiteID")["PVD"].count().reset_index(name='PVD_reOp')
+resultPVD = pd.merge(PVDOp, PVDReOp, on='SiteID', how='left')
+dfPVD =pd.merge(dfCancer, resultPVD, on='SiteID')
+
+dfPVD.to_csv("/tmp/pycharm_project_355/riskFactors.csv")
 # df=pd.read_csv("mortalty.csv")
 #reOp
 
 df['mortalPerReOp']=(df['Mortalty_SiteID_reOp']/df['countReOp'])*100
-df['prop']=df['countReOp/countOp']
+df['prop']=df['countReOp/countFirst+countReOp']
 
 #1
-df.plot(kind='scatter', x='totalOp', y='mortalPerReOp', title="Mortality of reOp - total Ops")
+df.plot(kind='scatter', x='countOpr', y='mortalPerReOp', title="Mortality of reOp - total Ops")
 plt.show()
 
 #2
@@ -105,47 +175,47 @@ df.plot(kind='scatter', x='countReOp', y='mortalPerReOp', title="Mortality of re
 plt.show()
 
 #3
-df.plot(kind='scatter', x='countReOp/countOp', y='mortalPerReOp', title="Mortality of reOp - reOp/(reOp+Ops)")
+df.plot(kind='scatter', x='countReOp/countFirst+countReOp', y='mortalPerReOp', title="Mortality of reOp - reOp/(reOp+Ops)")
 plt.show()
 
 ###oP
 #1
-df['mortalPerOp']=(df['Mortalty_SiteID_op']/df['countOp'])*100
-df.plot(kind='scatter', x='totalOp', y='mortalPerOp', title="Mortality of op - total Ops")
+df['mortalPerOp']=(df['Mortalty_SiteID_op']/df['countFirst'])*100
+df.plot(kind='scatter', x='countOpr', y='mortalPerOp', title="Mortality of op - total Ops")
 plt.show()
 
 #2
-df.plot(kind='scatter', x='countOp', y='mortalPerOp', title="Mortality of op - ops")
+df.plot(kind='scatter', x='countFirst', y='mortalPerOp', title="Mortality of op - ops")
 plt.show()
 
 #3
-df.plot(kind='scatter', x='countReOp/countOp', y='mortalPerOp', title="Mortality of op - reOp/(reOp+Ops)")
+df.plot(kind='scatter', x='countReOp/countFirst+countReOp', y='mortalPerOp', title="Mortality of op - reOp/(reOp+Ops)")
 plt.show()
 
 
 #spearman
 print("spearman")
 #reOp
-print(df.mortalPerReOp.corr(df.totalOp, method="spearman"))
+print(df.mortalPerReOp.corr(df.countOpr, method="spearman"))
 print(df.mortalPerReOp.corr(df.countReOp, method="spearman"))
 print(df.mortalPerReOp.corr(df.prop, method="spearman"))
 
 #op
-print(df.mortalPerOp.corr(df.totalOp, method="spearman"))
-print(df.mortalPerOp.corr(df.countOp, method="spearman"))
+print(df.mortalPerOp.corr(df.countOpr, method="spearman"))
+print(df.mortalPerOp.corr(df.countFirst, method="spearman"))
 print(df.mortalPerOp.corr(df.prop, method="spearman"))
 
 
 #pearson
 print("pearson")
 #reOp
-print(df.mortalPerReOp.corr(df.totalOp, method="pearson"))
+print(df.mortalPerReOp.corr(df.countOpr, method="pearson"))
 print(df.mortalPerReOp.corr(df.countReOp, method="pearson"))
 print(df.mortalPerReOp.corr(df.prop, method="pearson"))
 
 #op
-print(df.mortalPerOp.corr(df.totalOp, method="pearson"))
-print(df.mortalPerOp.corr(df.countOp, method="pearson"))
+print(df.mortalPerOp.corr(df.countOpr, method="pearson"))
+print(df.mortalPerOp.corr(df.countFirst, method="pearson"))
 print(df.mortalPerOp.corr(df.prop, method="pearson"))
 
-
+scipy.stats.linregress(xy)
