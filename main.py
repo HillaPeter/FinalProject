512f5ffb64a02b181576d725558c0b9fd78697ea
HillaPeter <peterhilla@gmail.com>
2020-11-03 12:11:44 +0200

    risk factors into csv

--- main.py
+++ main.py
@@ -111,12 +111,14 @@ genderReOp_grouped_female = (dfReOp.groupby("SiteID")["reOpGender_2.0"]).sum().r
 dfFemale=pd.merge(genderReOp_grouped_male, genderReOp_grouped_female, on='SiteID')
 dfGender=pd.merge(dfMale, dfFemale, on='SiteID')
 
+dfMerge=pd.merge(dfAge,dfGender, on='SiteID')
+
 
 ##FHCAD - family history of disease
 FHCADOp = dfOp.groupby("SiteID")["FHCAD"].count().reset_index(name='FHCAD_op')
 FHCADReOp = dfReOp.groupby("SiteID")["FHCAD"].count().reset_index(name='FHCAD_reOp')
 resultFHCAD = pd.merge(FHCADOp, FHCADReOp, on='SiteID', how='left')
-dfFHCAD =pd.merge(dfGender, resultFHCAD, on='SiteID')
+dfFHCAD =pd.merge(dfMerge, resultFHCAD, on='SiteID')
 
 ##Hypertn - blood preasure
 HypertnOp = dfOp.groupby("SiteID")["Hypertn"].count().reset_index(name='Hypertn_op')
